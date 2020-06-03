21746537c5245accb1473be868dd818f11ed6c42
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-02 23:46:19 -0300

    Rota exibição arquivos estáticos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express from 'express';
+import path from 'path';
 
 const app = express();
 import routes from './routes';
@@ -8,5 +9,6 @@ const { PORT = 3333 } = process.env;
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => console.log(`Running on port ${PORT} and updating`));
