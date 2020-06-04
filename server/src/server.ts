8de11cff8c2f486f88e42d43db14991ad7b76256
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-04 00:06:41 -0300

    Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,13 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 import routes from './routes';
 const { PORT = 3333 } = process.env;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
