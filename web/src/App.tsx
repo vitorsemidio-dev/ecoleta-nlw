6121f3b3964271a069decd1667730da389429bff
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-04 03:03:22 -0300

    Ajustes layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
+import React from 'react';
+import './App.css'
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
 
   return (
     <>
-      <Header title="Hello World" />
-      <Header title="Hello nlw" />
-      <h1>Hello nlw {counter}</h1>
-
-      <button type="button" onClick={handleButtonClick}>
-        Aumentar
-      </button>
+      <h1>Hello World</h1>
     </>
   );
 }
