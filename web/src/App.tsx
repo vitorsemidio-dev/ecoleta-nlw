a0e23808e40b9849c37740551c250ecd722aa5cf
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-04 02:48:43 -0300

    Estado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <>
       <Header title="Hello World" />
       <Header title="Hello nlw" />
-      <h1>Hello nlw</h1>
+      <h1>Hello nlw {counter}</h1>
+
+      <button type="button" onClick={handleButtonClick}>
+        Aumentar
+      </button>
     </>
   );
 }
