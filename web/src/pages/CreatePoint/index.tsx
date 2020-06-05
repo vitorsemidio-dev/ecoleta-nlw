69e4e5e79bbadc7651e49a074effff2320b53239
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-05 00:20:42 -0300

    Exibindo mapa

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
 import './CreatePoint.css';
 
 import logo from '../../assets/logo.svg';
@@ -59,6 +61,15 @@ const CreatePoint = () => {
               <h2>Endereço</h2>
               <span>Selecione o endereço no mapa</span>
             </legend>
+
+            <Map center={[ -22.8814484, -43.4864973 ]} zoom={15}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+              <Marker position={[ -22.8814484, -43.4864973 ]} />
+            </Map>
 
             <div className="field-group">
               <div className="field">
