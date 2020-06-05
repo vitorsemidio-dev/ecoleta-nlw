e7cd56c1b72b48738349c5e75053914867545111
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-04 23:51:01 -0300

    header create-point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './CreatePoint.css';
+
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
   return (
     <>
-      <h1>Hello Point</h1>
+      <div id="page-create-point">
+        <header>
+          <img src={logo} alt="Ecoleta"/>
+
+          <Link to="/">
+            <FiArrowLeft /> Voltar para home
+          </Link>
+        </header>
+
+        <form>
+          <h1>Cadastro do <br /> ponto de coleta</h1>
+        </form>
+      </div>
     </>
   )
 }
