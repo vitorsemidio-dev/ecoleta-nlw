dc074e36bad50c2cc177cbeed9241bc9b4596174
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-05 00:00:32 -0300

    Estrutura do campo "Dados"

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -20,6 +20,51 @@ const CreatePoint = () => {
 
         <form>
           <h1>Cadastro do <br /> ponto de coleta</h1>
+
+          <fieldset>
+            <legend>
+              <h2>Dados</h2>
+            </legend>
+            <div className="field">
+              <label htmlFor="name">Nome da Entidade</label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+              />
+            </div>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="email">E-mail</label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                />
+              </div>
+              <div className="field">
+                <label htmlFor="whastapp">WhatsApp</label>
+                <input
+                  id="whastapp"
+                  name="whastapp"
+                  type="text"
+                />
+              </div>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <h2>Endereço</h2>
+            </legend>
+          </fieldset>
+          <fieldset>
+            
+            <legend>
+              <h2>Ítens de coleta</h2>
+            </legend>
+          </fieldset>
         </form>
       </div>
     </>
