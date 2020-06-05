03aec5fd4fccaf4d4b61c136496c15edabe6462f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-04 23:07:31 -0300

    Estilos home movidos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css'
 
+import Home from './pages/Home';
+
 function App() {
 
   return (
     <>
+      <Home />
       <h1>Hello World</h1>
     </>
   );
