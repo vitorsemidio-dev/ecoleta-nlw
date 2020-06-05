5ca18fa4491a34938635413a0a6e604ff45768c6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-05 00:08:55 -0300

    Listagem de itens

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -57,14 +57,62 @@ const CreatePoint = () => {
           <fieldset>
             <legend>
               <h2>Endereço</h2>
+              <span>Selecione o endereço no mapa</span>
             </legend>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="uf">Estado (UF)</label>
+                <select name="uf" id="uf">
+                  <option value="0">Selecione uma UF</option>
+                </select>
+              </div>
+              <div className="field">
+                <label htmlFor="city">Cidade</label>
+                <select name="city" id="city">
+                  <option value="0">Selecione uma cidade</option>
+                </select>
+              </div>
+            </div>
           </fieldset>
           <fieldset>
             
             <legend>
               <h2>Ítens de coleta</h2>
+              <span>Selecione um ou mais ítens abaixo</span>
             </legend>
+
+            <ul className="items-grid">
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="tmp"/>
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="tmp"/>
+                <span>Lâmpadas</span>
+              </li>
+              <li className="selected">
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="tmp"/>
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="tmp"/>
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="tmp"/>
+                <span>Lâmpadas</span>
+              </li>
+              <li>
+                <img src="http://localhost:3333/uploads/lampadas.svg" alt="tmp"/>
+                <span>Lâmpadas</span>
+              </li>
+            </ul>
           </fieldset>
+
+          <button type="submit">
+            Cadastrar ponto de coleta
+          </button>
         </form>
       </div>
     </>
