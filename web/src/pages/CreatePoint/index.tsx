1a3ae7d536a29fa566ba669ed51a2036df7bd01c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-05 02:07:41 -0300

    Definição localização usuário

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -32,6 +32,7 @@ const CreatePoint = () => {
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
   useEffect(() => {
     api.get('/items').then(({ data }) => {
@@ -59,6 +60,13 @@ const CreatePoint = () => {
         setCityNames(cityNames);
       });
   }, [selectedUf]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
 
   function handleSeletedUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value;
@@ -128,7 +136,7 @@ const CreatePoint = () => {
               <span>Selecione o endereço no mapa</span>
             </legend>
 
-            <Map center={[ -22.8814484, -43.4864973 ]} zoom={15} onClick={handleMapClick}>
+            <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
               <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
