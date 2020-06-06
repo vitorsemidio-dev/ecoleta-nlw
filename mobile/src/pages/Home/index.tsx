e7388ac03acbf31938992a059d3699e25b6f9ccf
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-06 01:36:48 -0300

    Carregamento fontes e copia estilos

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,12 +1,85 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
 const Home = () => {
   return (
-    <View >
+    <View style={styles.container} >
       <Image source={require('../../assets/logo.png')} />
+      <Text>
+        Reloading
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: '#6C6C80'
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#F0F0F5',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: '#83838e',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#F0F0F5',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  }
+});
 
 export default Home;
