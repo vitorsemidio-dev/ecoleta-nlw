82e3fe14b8968a23001553d38be3969efcd9f824
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-06 02:22:33 -0300

    Configuração de rotas

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -31,7 +31,8 @@ const Home = () => {
         >
           <View style={styles.buttonIcon}>
             <Text>
-              <Icon name="arrow-right" color="#FFF" size={24} />
+              {/* <Icon name="arrow-right" color="#FFF" size={24} /> */}
+              Icon
             </Text>
           </View>
           <Text style={styles.buttonText}>
