21be63b0ad41077b798d8a37aff5eeda00af3daa
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-07 12:48:41 -0300

    Validação com Celebrate

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import ItemController from './app/controllers/ItemController';
 import PointController from './app/controllers/PointController';
@@ -14,6 +15,25 @@ routes.post('/items', ItemController.store);
 routes.get('/points', PointController.index);
 routes.get('/points/:id', PointController.show);
 routes.delete('/points/:id', PointController.delete);
-routes.post('/points', upload.single('image'), PointController.store);
+
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+      items: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  PointController.store
+);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 import routes from './routes';
@@ -12,5 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, () => console.log(`Running on port ${PORT} and updating`));
