6193b0c11ac185aea82683beb298a9c7de1b16f3
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-07 09:57:46 -0300

    Salvando nome da imagem

--- server/src/app/controllers/PointController.ts
+++ server/src/app/controllers/PointController.ts
@@ -59,7 +59,7 @@ class PointController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: req.file.filename,
       name,
       email,
       whatsapp,
